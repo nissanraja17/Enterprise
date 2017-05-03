542926e812d5897140b93babb07154a9baf58cfb
Nissan <nishanraja17@gmail.com>
2017-05-03 17:08:19 +0530

    first commit

---
+++ src/main/java/com/springlegacy/SpringController.java
@@ -0,0 +1,14 @@
+package com.springlegacy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SpringController {
+	
+	@RequestMapping("/hello")
+	public ModelAndView get() {
+		return new ModelAndView("home", "message", "welcome");
+	}	
+}
